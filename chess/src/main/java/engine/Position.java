894436727073ea1dbe9f684fd21614982bded663
mang0r <benoit.jaouen@heig-vd.ch>
2024-12-12 17:27:12 +0100

    Une implementation de position que l'on pourrait utiliser à la place d'une Pair<Integer, Integer> vu que Pair a l'air d'appartenir à une autre package, et qu'un .x et .y pourraient être plus ergnonomiques pour accéder aux attributs.

---
+++ chess/src/main/java/engine/Position.java
@@ -0,0 +1,15 @@
+package engine;
+
+public class Position {
+    public final int x;
+    public final int y;
+
+    public Position (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Position next(Direction d) {
+        return new Position(x + d.dx, y + d.dy);
+    }
+}
