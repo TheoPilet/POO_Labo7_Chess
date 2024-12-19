68caac8c0d886054d3e8f134ea42883af2ea08c9
Bénédicte Vernet <benedicte.vernet@arcanite.ch>
2024-12-19 14:21:41 +0100

    change position

--- chess/src/main/java/engine/pieces/Bishop.java
+++ chess/src/main/java/engine/pieces/Bishop.java
@@ -2,11 +2,15 @@ package engine.pieces;
 
 import chess.PlayerColor;
 import engine.ChessGame;
+import engine.Move;
 import engine.Piece;
+import engine.utils.*;
 
 public class Bishop extends Piece {
     
     public Bishop (PlayerColor color, ChessGame chessGame) {
         super(color, chessGame);
     }
+
+    
 }
