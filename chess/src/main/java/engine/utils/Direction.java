bf740d5f8fa6dd383cde99120a71b3ccd1a98810
mang0r <benoit.jaouen@heig-vd.ch>
2025-01-06 15:42:20 +0100

    changed Direction to class instead of enum

--- chess/src/main/java/engine/utils/Direction.java
+++ chess/src/main/java/engine/utils/Direction.java
@@ -1,24 +1,24 @@
 package engine.utils;
 
-public enum Direction {
-    UP(0, 1),
-    UP_LEFT(-1, 1),
-    LEFT(-1, 0),
-    DOWN_LEFT(-1, -1),
-    DOWN(0, -1),
-    DOWN_RIGHT(1, -1),
-    RIGHT(1, 0),
-    UP_RIGHT(1, 1);
-
+public class Direction {
     public final int dx;
     public final int dy;
 
-    private Direction (int dx, int dy) {
-        this.dx = dx;
-        this.dy = dy;
-    }
+    public static final Direction UP = new Direction(0, 1);
+    public static final Direction UP_LEFT = new Direction(-1, 1);
+    public static final Direction LEFT = new Direction(-1, 0);
+    public static final Direction DOWN_LEFT = new Direction(-1, -1);
+    public static final Direction DOWN = new Direction(0, -1);
+    public static final Direction DOWN_RIGHT = new Direction(1, -1);
+    public static final Direction RIGHT = new Direction(1, 0);
+    public static final Direction UP_RIGHT = new Direction(1, 1);
 
     public static final Direction[] STRAIGHT_DIRECTIONS = {UP, LEFT, DOWN, RIGHT};
     public static final Direction[] DIAGONAL_DIRECTIONS = {UP_LEFT, DOWN_LEFT, DOWN_RIGHT, UP_RIGHT};
     public static final Direction[] ALL_DIRECTIONS = {UP, LEFT, DOWN, RIGHT, UP_LEFT, DOWN_LEFT, DOWN_RIGHT, UP_RIGHT};
+
+    public Direction(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
 }
