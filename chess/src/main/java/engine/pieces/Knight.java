ce2a4d1eead0b317cf2d9b9806c476c70eb23817
mang0r <benoit.jaouen@heig-vd.ch>
2025-01-06 16:33:35 +0100

    small upgrade 2

--- chess/src/main/java/engine/pieces/Knight.java
+++ chess/src/main/java/engine/pieces/Knight.java
@@ -23,16 +23,14 @@ public class Knight extends Piece{
     public LinkedList<Move> availableMoves(){
 
         LinkedList<Move> availableMoves = new LinkedList<>();
-
         Position from = chessGame.where(this);
-
-        Direction[] directions = {  new Direction(1, 2),
-                            new Direction(-1, 2),
-                            new Direction(1, -2),
-                            new Direction(-1, -2)
-                        };
         
-        for(Direction dir : directions){
+        for(Direction dir : new Direction[]{
+            new Direction(1, 2),
+            new Direction(-1, 2),
+            new Direction(1, -2),
+            new Direction(-1, -2)
+        }) {
             availableMoves.addAll(getMovesInDirection(dir, 1, from));
         }
         
