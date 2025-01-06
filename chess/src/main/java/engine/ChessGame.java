5bc414461e87d822c2db00e0daa0477b73c1fab4
mang0r <benoit.jaouen@heig-vd.ch>
2025-01-06 15:29:16 +0100

    merge with feature-ChessGame

--- chess/src/main/java/engine/ChessGame.java
+++ chess/src/main/java/engine/ChessGame.java
@@ -6,12 +6,15 @@ import java.util.LinkedList;
 import chess.ChessController;
 import chess.ChessView;
 import chess.PlayerColor;
-import engine.pieces.Bishop;
 import engine.pieces.King;
+<<<<<<< HEAD
 import engine.pieces.Knight;
 import engine.pieces.Pawn;
 import engine.pieces.Queen;
+=======
+>>>>>>> cd2d9dc (isThreatened implemented, board initialisation moved to another filed.)
 import engine.pieces.Rook;
+import engine.utils.ChessBoardInitializer;
 import engine.utils.Position;
 
 public class ChessGame implements ChessController {
@@ -26,6 +29,8 @@ public class ChessGame implements ChessController {
 
 	private Piece whiteKing;
 	private Piece blackKing;
+	private PlayerColor currentPlayerColor = PlayerColor.WHITE;
+
 
 	@Override
 	public void start(ChessView view) {
@@ -38,6 +43,7 @@ public class ChessGame implements ChessController {
 		System.out.println(String.format("TO REMOVE : from (%d, %d) to (%d, %d)", fromX, fromY, toX, toY)); // TODO remove
 		Piece p = board[fromX][fromY];
 		if (p == null) return false; // no piece to move at the start position
+		if (p.getColor() != currentPlayerColor) return false;
 
 		Position from = new Position(fromX, fromY);
 		Position to = new Position(toX, toY);
@@ -46,6 +52,7 @@ public class ChessGame implements ChessController {
 		if (move == null) return false;
 
 		applyMove(move);
+
 		if (!isStateValid()) {
 			revertLastMove();
 			return false;
@@ -54,16 +61,19 @@ public class ChessGame implements ChessController {
 		//TODO : check promotion
 
 		uppdateBoardView(move);
+
+		nextTurn();
 		return true;
 	}
 
 	public Move getMoveIfAllowed(Piece p, Position from, Position to) {
-		for (Move m : p.availableMoves()) {
+		return new Move(p, new Rook(PlayerColor.WHITE, this), from, to); // test code
+		/*for (Move m : p.availableMoves()) {
 			if (m.from.equals(from) && m.to.equals(to)) {
 				return m;
 			}
 		}
-		return null;
+		return null;*/
 	}
 
 	private void applyMove (Move m) {
@@ -83,24 +93,35 @@ public class ChessGame implements ChessController {
 	}
 
 	private boolean isStateValid() {
-		//TODO: implement
-		return false;
+		return isThreatenend((currentPlayerColor == PlayerColor.WHITE) ? whiteKing : blackKing);
 	}
 
-	public boolean isThreatenend(Position p) {
-		//TODO: implement
-		return false;
+	private void nextTurn() {
+		currentPlayerColor = PlayerColor.values()[(currentPlayerColor.ordinal() + 1) % PlayerColor.values().length];
+	}
+
+	public boolean isThreatenend(Piece p) { // TODO: remove if not used
+		return Arrays.stream(board)
+			.flatMap(Arrays::stream)
+			.flatMap(piece -> piece.availableMoves().stream())
+			.anyMatch(m -> m.pieceEaten == p);
+	}
+
+	public boolean isThreatened(Position p) { // TODO: remove if not used
+		return Arrays.stream(board)
+			.flatMap(Arrays::stream)
+			.flatMap(piece -> piece.availableMoves().stream())
+			.anyMatch(m -> m.to.equals(p));
 	}
 
 	public boolean hasMoved(Piece p) {
-		//TODO: implement
-		return false;
+		return history.stream().anyMatch(m -> m.pieceMoved == p);
 	}
 
 	@Override
 	public void newGame() {
 		resetBoard();
-		initializeBoard();
+		board = ChessBoardInitializer.standardInitializedBoard(this);
 		updateBoardView();
 	}
 
@@ -135,6 +156,7 @@ public class ChessGame implements ChessController {
 		&& p.y() < HEIGHT;
 	}
 
+<<<<<<< HEAD
 	private void initializeBoard () {
 	PlayerColor white = PlayerColor.WHITE;
 		PlayerColor black = PlayerColor.BLACK;
@@ -175,6 +197,8 @@ public class ChessGame implements ChessController {
 		for (int x=0, y=HEIGHT-2; x < WIDTH; ++x) board[x][y] = new Pawn(black, this);
 	}
 
+=======
+>>>>>>> cd2d9dc (isThreatened implemented, board initialisation moved to another filed.)
 	private void resetBoard() {
 		for (int x=0; x < WIDTH; ++x) {
 			for (int y=0; y < HEIGHT; ++y) {
@@ -197,5 +221,18 @@ public class ChessGame implements ChessController {
 		view.removePiece(m.from.x(), m.from.y());
 		if (m.pieceEaten != null) view.removePiece(m.to.x(), m.to.y()); // TODO: necessary ?
 		view.putPiece(m.pieceMoved.getType(), m.pieceMoved.getColor(), m.to.x(), m.to.y());
+	}
+	
+	public void setWhiteKing(King whiteKing) {
+		this.whiteKing = whiteKing;
+	}
+	public void setBlackKing(King blackKing) {
+		this.blackKing = blackKing;
+	}
+	public int width() {
+		return WIDTH;
+	}
+	public int height() {
+		return HEIGHT;
 	}
 }
