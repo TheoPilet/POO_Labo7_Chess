ed406f2d1e3b161792ce92ccf7f2cd69dbc4f9ea
Bénédicte Vernet <benedicte.vernet@arcanite.ch>
2025-01-09 00:13:07 +0100

    javadoc for all pieces

--- chess/src/main/java/engine/pieces/Bishop.java
+++ chess/src/main/java/engine/pieces/Bishop.java
@@ -10,17 +10,34 @@ import engine.Piece;
 import engine.utils.Direction;
 import engine.utils.Position;
 
+/**
+ * Defines the class Bishop that inherits from Piece
+ * @author Bénédicte Vernet & Benoît Jaouen & Théo Pilet
+ */
 public class Bishop extends Piece {
     
+    /**
+     * Creates a constructor of Bishop
+     * @param color    the color of the piece
+     * @param chessGame     the chess game we're playing with
+     */
     public Bishop (PlayerColor color, ChessGame chessGame) {
         super(color, chessGame);
     }
 
+    /**
+     * Getter to know the type of the piece
+     * @return  the PieceType BISHOP
+     */
     @Override
     public PieceType getType(){
         return PieceType.BISHOP;
     }
 
+    /**
+     * Method that returns all the moves that the piece can do
+     * @return  a LinkedList of Moves 
+     */
     @Override
     public LinkedList<Move> availableMoves(){
         Position from = chessGame.where(this);
